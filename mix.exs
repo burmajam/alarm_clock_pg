a5daa07e6f61da886c4145b8ebd837382c801ab9
Milan Burmaja <burmajam@gmail.com>
2017-03-14 14:18:21 +0100

    bump version to 0.0.4

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule AlarmClock.PG.Mixfile do
 
   def project do
     [app: :alarm_clock_pg,
-     version: "0.0.3",
+     version: "0.0.4",
      elixir: "~> 1.3",
      package: package(),
      description: description(),
